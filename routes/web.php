1d52b6d9d7a12bd93d6f3cae96ebdd349f2d860a
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2017-09-25 23:21:44 +0500

    Adding static view and dynamic routes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/', function () {
    //return "Hello World!";
 });
 
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
 Route::get('/hello', function () {
     //return view('welcome');
     return "Hello World!";
@@ -24,4 +28,16 @@ Route::get('/hello', function () {
 Route::get('/html', function () {
     //return view('welcome');
     return "<h1>Hello World!</h1>";
+});
+
+//Dynamic Route Example 1
+Route::get('/users/{id}', function ($id) {
+//    return "This is User $id";
+    return 'This is User ' . $id;
+});
+
+//Dynamic Route Example 2
+Route::get('/users/{id}/{name}', function ($id,$name) {
+   return "This is User $name with an ID of $id";
+//    return 'This is User ' . $name . ' with an ID of ' . $id;
 });
