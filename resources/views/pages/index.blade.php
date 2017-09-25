7bd8ceab2eca8795d6e0ca6fe337ec17cd4166be
Ali-Shaikh <aliakbarshaikh@live.co.uk>
2017-09-26 01:09:23 +0500

    Passing values into a blade template

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name', 'LSAPP')}}</title>
+@extends('layouts.app')
 
-</head>
-<body>
-<h1>Welcome to Laravel - Ali Shaikh</h1>
-<p>This is the laravel app for the YouTube series "Laravel From Scratch".</p>
-</body>
-</html>
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>This is the laravel app for the YouTube series "Laravel From Scratch".</p>
+@endsection
